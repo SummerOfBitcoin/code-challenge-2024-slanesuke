460c733cc9ecc1ff4d3e1843cca3cc43300149cc
Ian Slane <slaneian@gmail.com>
2024-04-16 15:01:43 -0600

    test

--- mine-your-first-block/src/main.rs
+++ mine-your-first-block/src/main.rs
@@ -244,9 +244,10 @@ fn get_merkle_root(txids: Vec<String>) -> String {
             }).collect()
     }
     // The last hash in the merkle tree
-    // merkle root
-    hex::encode(be_txid[0])
-
+    // merkle root in little endian
+    let merkle_root = be_txid[0].to_vec();
+    let merkle_root_be = merkle_root.iter().rev().cloned().collect::<Vec<u8>>();
+    hex::encode(merkle_root_be)
 }
 
 fn deserialize_tx(filename: &str) -> Transaction {
