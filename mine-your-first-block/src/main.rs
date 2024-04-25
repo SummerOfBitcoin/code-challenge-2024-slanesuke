c945e6748dc6fad2a2d663ae2b08b509d5b212c3
Ian Slane <slaneian@gmail.com>
2024-04-24 19:43:24 -0600

    Testing the increase of my weight limit once more

--- mine-your-first-block/src/main.rs
+++ mine-your-first-block/src/main.rs
@@ -1229,7 +1229,9 @@ fn main() {
     // Initializing block weight
     let mut block_txs: Vec<TransactionForProcessing> = Vec::new();
     let mut total_weight = 0u64;
-    let max_block_weight = 5000000u64;
+    // My calculate_transaction_weight function is off so I increased the weight limit for a quick fix
+    // FIX calc_tx_weight
+    let max_block_weight = 7000000u64;
     let mut total_fees = 0u64;
 
     // Sort transactions by fee in descending order before processing
